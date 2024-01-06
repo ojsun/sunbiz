a74c1032709715e0e1764588fa3f2ddf281fd9bd
오재선 <soddongmo@naver.com>
2024-01-06 20:10:55 +0900

    test

--- config/settings.py
+++ config/settings.py
@@ -77,6 +77,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
