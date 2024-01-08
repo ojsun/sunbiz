ab4ff867316cafdcd204c0345cb7ce80273b5015
ojsun <soddongmo@naver.com>
2024-01-08 22:11:06 +0900

    ..

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -83,10 +82,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "sunbiz_db",
-        "USER": "root",
+        "USER": "maria",
         "PASSWORD": "1234",
-        "HOST": "127.0.0.1",
-        "PORT": 3306,
+        "HOST": "svc.sel5.cloudtype.app",
+        "PORT": "31731",
     }
 }
 
